00a34a1b2d85fd6103a0a8adcf80ba2cce7961c5
Deepthi Ushus <ushusdeepthi@gmail.com>
2020-11-05 14:02:57 +0100

    grid

---
+++ cv.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded",function(e){
+    let about_details=document.getElementById("abou_details")
+    let education=document.getElementById("education");
+    education.addEventListener("click",()=>{
+        document.getElementById("dec").remove();
+        let edu_des=document.createElement("p");
+       
+    })
+})
