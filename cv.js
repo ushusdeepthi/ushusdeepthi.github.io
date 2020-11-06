513684a1267d2eca9123aa39018fee227af2060d
Deepthi Ushus <ushusdeepthi@gmail.com>
2020-11-06 15:56:36 +0100

    modified and changed js,scss,index

--- cv.js
+++ cv.js
@@ -1,9 +1,17 @@
-document.addEventListener("DOMContentLoaded",function(e){
-    let about_details=document.getElementById("abou_details")
-    let education=document.getElementById("education");
-    education.addEventListener("click",()=>{
-        document.getElementById("dec").remove();
-        let edu_des=document.createElement("p");
-       
+document.addEventListener("DOMContentLoaded",()=>{
+    let me=document.getElementById("me");
+    let course=document.getElementById("course");
+    me.addEventListener("click",(e)=>{
+        window.location.reload();
+
     })
+    course.addEventListener("click",(e)=>{
+       document.getElementById("image").src="./images/nackademin.png"
+        let about=document.getElementById("about_desc");
+        about.remove();
+        let course_desc=document.createElement("p");
+        course_desc.textContent="Course at nackademin";
+        me.before(course_desc);  
+    })
+    
 })
